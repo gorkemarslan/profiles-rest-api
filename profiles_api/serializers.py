11b932a623d411a189d24102b3eedcce4c9cc235
grkm <garslan13@gmail.com>
2021-02-01 23:29:44 +0300

    Added update function to serializer.py

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -28,3 +28,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
             password = validated_data['password'],
         )
         return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
