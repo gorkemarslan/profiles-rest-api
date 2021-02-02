8e2efc02397a6d0e97a77c61b61925c8cfbe19f4
grkm <garslan13@gmail.com>
2021-02-02 22:55:31 +0300

    Added authentication and permission to user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
 from profiles_api import models
+from profiles_api import serializers
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test Api View"""
@@ -96,3 +99,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profile"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
