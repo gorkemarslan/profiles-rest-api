86b3795834bf80c0858aa5537edf61e45779cf17
grkm <garslan13@gmail.com>
2021-02-04 19:42:32 +0300

    ProfileFeedItemSerializer is added.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -59,3 +59,9 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 class ProfileFeedItem(models.Model):
     """Profile status update"""
     user_profile = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
